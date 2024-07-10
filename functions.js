9cced394490ce07bf3d208cd45c63dddbfdcf9de
chuantt <chuan123789@gmail.com>
2024-07-10 16:18:06 +0700

    init

---
+++ functions.js
@@ -0,0 +1,33 @@
+import UserAgent from "user-agents";
+
+export const getHeader = () => {
+  const userAgent = new UserAgent();
+  const headers = {
+    "sec-fetch-user": "?1",
+    "sec-ch-ua-mobile": "?0",
+    "sec-fetch-site": "none",
+    "sec-fetch-dest": "document",
+    "sec-fetch-mode": "navigate",
+    "cache-control": "max-age=0",
+    authority: "t11.freetranslations.org",
+    "upgrade-insecure-requests": "1",
+    "accept-language": "en-GB,en;q=0.9,tr-TR;q=0.8,tr;q=0.7,en-US;q=0.6",
+    "user-agent": userAgent.toString(),
+    accept:
+      "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+  };
+  return headers
+};
+
+export const sliceExtendFile = (name) => {
+  let str = "";
+
+  if (name) {
+    const newArrName = String(name).split("/");
+    if (newArrName.length > 1) {
+      str = newArrName[newArrName.length - 1];
+    }
+  }
+
+  return str;
+};
---
+++ index.js
@@ -0,0 +1,44 @@
+import axios from "axios";
+import express from "express";
+import { getHeader, sliceExtendFile } from "./functions.js";
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/translation", async (req, res) => {
+  const header = getHeader();
+  const query = req.query;
+  const url = new URL(
+    "https://t11.freetranslations.org/freetranslationsorg.php"
+  );
+  url.searchParams.append("p1", query?.p1);
+  url.searchParams.append("p2", query?.p2);
+  url.searchParams.append("p3", query?.p3);
+  const data = await axios.get(url, {
+    headers: header,
+  });
+  return res.send(data.data);
+});
+
+app.get("/speak", async (req, res) => {
+  const header = getHeader();
+  const query = req.query;
+  const url = new URL(
+    "https://www.freetranslations.org/speak.php"
+  );
+  url.searchParams.append("word", query?.word);
+  url.searchParams.append("lang", query?.lang);
+  const data = await axios.get(url, {
+    headers: header,
+    responseType: "arraybuffer"
+  });
+  const extension = sliceExtendFile(data.headers.get("Content-Type"));
+  if (extension) {
+    // res.header("Content-Type", data.headers.get("Content-Type"));
+    // const download = Buffer.from(data.data, "base64");
+    return res.sendFile(data.data);
+  }
+});
+
+app.listen(3001);
