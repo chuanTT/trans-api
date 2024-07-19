cad701a8bee5e9109dc1c245fd7351c969baad41
chuantt <chuan123789@gmail.com>
2024-07-19 08:22:27 +0700

    update

--- functions.js
+++ functions.js
@@ -1,7 +1,66 @@
-import UserAgent from "user-agents";
 
-export const getHeader = () => {
-  const userAgent = new UserAgent();
+export const funTmpnum = (target_lang) => {
+  let tmpnum;
+  do {
+    if (
+      target_lang == "ay" ||
+      target_lang == "bm" ||
+      target_lang == "be" ||
+      target_lang == "bn" ||
+      target_lang == "bho" ||
+      target_lang == "ceb" ||
+      target_lang == "co" ||
+      target_lang == "dv" ||
+      target_lang == "doi" ||
+      target_lang == "eo" ||
+      target_lang == "ee" ||
+      target_lang == "fy" ||
+      target_lang == "gn" ||
+      target_lang == "ha" ||
+      target_lang == "haw" ||
+      target_lang == "ig" ||
+      target_lang == "ilo" ||
+      target_lang == "jv" ||
+      target_lang == "rw" ||
+      target_lang == "gom" ||
+      target_lang == "kri" ||
+      target_lang == "ckb" ||
+      target_lang == "la" ||
+      target_lang == "ln" ||
+      target_lang == "lg" ||
+      target_lang == "lb" ||
+      target_lang == "mai" ||
+      target_lang == "mni-Mtei" ||
+      target_lang == "lus" ||
+      target_lang == "ny" ||
+      target_lang == "or" ||
+      target_lang == "om" ||
+      target_lang == "qu" ||
+      target_lang == "sa" ||
+      target_lang == "gd" ||
+      target_lang == "nso" ||
+      target_lang == "st" ||
+      target_lang == "sn" ||
+      target_lang == "sd" ||
+      target_lang == "si" ||
+      target_lang == "su" ||
+      target_lang == "tg" ||
+      target_lang == "ti" ||
+      target_lang == "ts" ||
+      target_lang == "ak" ||
+      target_lang == "xh" ||
+      target_lang == "yi" ||
+      target_lang == "yo"
+    ) {
+      tmpnum = Math.floor(Math.random() * 12) + 1;
+    } else {
+      tmpnum = Math.floor(Math.random() * 18) + 1;
+    }
+  } while ([17, 18, 15, 14, 13].includes(tmpnum));
+  return tmpnum;
+};
+
+export const getHeader = (domain) => {
   const headers = {
     "sec-fetch-user": "?1",
     "sec-ch-ua-mobile": "?0",
@@ -9,10 +68,9 @@ export const getHeader = () => {
     "sec-fetch-dest": "document",
     "sec-fetch-mode": "navigate",
     "cache-control": "max-age=0",
-    authority: "t11.freetranslations.org",
+    authority: domain,
     "upgrade-insecure-requests": "1",
     "accept-language": "en-GB,en;q=0.9,tr-TR;q=0.8,tr;q=0.7,en-US;q=0.6",
-    "user-agent": userAgent.toString(),
     accept:
       "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
   };
--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 import axios from "axios";
+import cors from "cors";
 import express from "express";
-import { getHeader, sliceExtendFile } from "./functions.js";
+import { funTmpnum, getHeader } from "./functions.js";
 const app = express();
+
+app.use(cors())
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/translation", async (req, res) => {
-  const header = getHeader();
   const query = req.query;
+  const domain = `t${funTmpnum(query?.p2)}.freetranslations.org`
+  const header = getHeader(domain);
   const url = new URL(
-    "https://t11.freetranslations.org/freetranslationsorg.php"
+    `https://${domain}/freetranslationsorg.php`
   );
   url.search = new URLSearchParams(query);
   const data = await axios.get(url, {
     headers: header,
     responseType: "text",
   });
+  console.log(domain)
   return res.send(data.data);
 });
 
